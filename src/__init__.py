b4980329cd99b9b86d61b700be9c1fe0ace4d3ac
Joe Santhosh <joe.santhosh88@yahoo.com>
2023-11-30 10:42:14 -0500

    Added user db model and added sql scripts to create databases

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
@@ -7,6 +8,19 @@ api = Api(app)
 
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
+    username = db.Column(db.String(128), nullable=False) 
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def  __init__(self, username, email): 
+        self.username = username
+        self.email = email
 
 class Ping(Resource):
     def get(self):
