36ca42176680fdfc1f6977ba99e128d21430f1e4
Joe Santhosh <joe.santhosh88@yahoo.com>
2023-11-30 10:15:41 -0500

    Removed print statement

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
 import os
-import sys
 
 app = Flask(__name__)
 api = Api(app)
 
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
-
-print(app.config, file=sys.stderr)
 
 class Ping(Resource):
     def get(self):
