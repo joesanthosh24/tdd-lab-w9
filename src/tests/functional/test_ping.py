e121d1d1a4e4bebdff957af2843b16314b8e5bd0
Joe Santhosh <joe.santhosh88@yahoo.com>
2023-11-30 11:49:01 -0500

    Made test_ping follow Given-When-Then framework

--- src/tests/functional/test_ping.py
+++ src/tests/functional/test_ping.py
@@ -1,9 +1,14 @@
 import json
 
 def test_ping(test_app):
-    client = test_app.test_client() 
+    # Given
+    client = test_app.test_client()
+
+    # When
     resp = client.get('/ping') 
-    data = json.loads(resp.data.decode()) 
+    data = json.loads(resp.data.decode())
+
+    # Then
     assert resp.status_code == 200
     assert 'pong' in data['message'] 
     assert 'success' in data['status']
